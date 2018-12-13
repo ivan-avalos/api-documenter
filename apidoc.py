894027b9bbc28731a768a16a1847e47aa642c829
avalos <ivan.avalos.diaz@hotmail.com>
2018-12-12 23:24:00 -0600

    New request/response body formatting using Google Code Prettify.

--- apidoc.py
+++ apidoc.py
@@ -108,10 +108,47 @@ if len(sys.argv) < 3:
 input_file=open(sys.argv[1], 'r')
 output_file=open(sys.argv[2], 'w')
 
+code_style="""
+    /* desert scheme ported from vim to google prettify */
+    pre.prettyprint { display: block; background-color: #333 }
+    pre .nocode { background-color: none; color: #000 }
+    pre .str { color: #ffa0a0 } /* string  - pink */
+    pre .kwd { color: #f0e68c; font-weight: bold }
+    pre .com { color: #87ceeb } /* comment - skyblue */
+    pre .typ { color: #98fb98 } /* type    - lightgreen */
+    pre .lit { color: #cd5c5c } /* literal - darkred */
+    pre .pun { color: #fff }    /* punctuation */
+    pre .pln { color: #fff }    /* plaintext */
+    pre .tag { color: #f0e68c; font-weight: bold } /* html/xml tag    - lightyellow */
+    pre .atn { color: #bdb76b; font-weight: bold } /* attribute name  - khaki */
+    pre .atv { color: #ffa0a0 } /* attribute value - pink */
+    pre .dec { color: #98fb98 } /* decimal         - lightgreen */
+    
+    /* Specify class=linenums on a pre to get line numbering */
+    ol.linenums { margin-top: 0; margin-bottom: 0; color: #AEAEAE } /* IE indents via margin-left */
+    li.L0,li.L1,li.L2,li.L3,li.L5,li.L6,li.L7,li.L8 { list-style-type: none }
+    /* Alternate shading for lines */
+    li.L1,li.L3,li.L5,li.L7,li.L9 { }
+    
+    @media print {
+    pre.prettyprint { background-color: none }
+    pre .str, code .str { color: #060 }
+    pre .kwd, code .kwd { color: #006; font-weight: bold }
+    pre .com, code .com { color: #600; font-style: italic }
+    pre .typ, code .typ { color: #404; font-weight: bold }
+    pre .lit, code .lit { color: #044 }
+    pre .pun, code .pun { color: #440 }
+    pre .pln, code .pln { color: #000 }
+    pre .tag, code .tag { color: #006; font-weight: bold }
+    pre .atn, code .atn { color: #404 }
+    pre .atv, code .atv { color: #060 }
+    }
+"""
+
 # Input data
 json_apidoc=json.loads(input_file.read())
 # Output HTML DOM
-html_apidoc=htmldom.HtmlDom().createDom('<html><head></head><body style="background: #f5f8fa"></body></html>')
+html_apidoc=htmldom.HtmlDom().createDom('<html><head><meta charset="utf-8"><link href="https://raw.githubusercontent.com/google/code-prettify/master/styles/desert.css"></head><body style="background: #f5f8fa"><style>'+code_style+'</style></body><script src="https://cdn.rawgit.com/google/code-prettify/master/loader/run_prettify.js"></script></html>')
 
 # Base URL
 if 'host' in json_apidoc:
@@ -236,13 +273,13 @@ for i in json_apidoc['requests']:
             # Example Body
             if 'body' in j:
                 if type(j['body']) is str:
-                    dom_example_table.append("<tr><td><code style='display:block; white-space: pre-line;'>"+j['body']+"</code></td></tr>")
+                    dom_example_table.append("<tr><td><code><pre class='prettyprint'>\b\n"+j['body']+"</pre></code></td></tr>")
                 elif type(j['body']) is list:
-                    body = ''
+                    body = '\b\n'
                     for i in j['body']:
                         body += i + '\n'
                     body = body[:-1]
-                    dom_example_table.append("<tr><td><code style='display:block; white-space: pre-line;'>"+body+"</code></td></tr>")
+                    dom_example_table.append("<tr><td><code><pre class='prettyprint'>"+body+"</pre></code></td></tr>")
 
 # Footer
 dom_body.append('''<footer><div class="container py-5">
